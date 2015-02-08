c5834ae7a3fff3e2a20d3562f35b6de505f7e0d0
Adam Schay <adamschay@gmail.com>
2015-02-07 17:26:05 -0700

    Cleanup whitespace and convert tab characters to spaces for consistency

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,49 @@
 $(document).ready(function () {
-  
+
   var socket = io.connect();
-  
+
   $("#makememe").click(function () {
-  
+
     var fileinput = document.getElementById('userIMG');
-  
+
     if ($("#toptext").val() && $("#bottext").val() && fileinput.files.length != 0) {
-    
+
       var f = fileinput.files[0];
-    
+
       console.log(f.name);
-  
+
       var reader = new FileReader();
-  
+
       reader.onload = function (e) {
         socket.emit('upload', {
-          base64: e.target.result, 
+          base64: e.target.result,
           filename: f.name,
-          toptext: $("#toptext").val(), 
-          bottext: $("#bottext").val(), 
-          fontcolor: $("#fontcolor").val(), 
+          toptext: $("#toptext").val(),
+          bottext: $("#bottext").val(),
+          fontcolor: $("#fontcolor").val(),
           fontbordercolor: $("#fontbordercolor").val(),
           fontsize: $("#fontsize").val()
         });
       }
-  
+
       reader.readAsDataURL(f);
-  
+
     }
-    
+
     socket.on("uploadedpicinfo", function (data) {
-      
+
       setTimeout(function () {
         $("#memeviewer").html('<img id="tempic" src="/images/outputpics/' + data.filename + '">');
 
         $("#tempic").attr("class", "centerer img-polaroid");
       }, 1000)
-      
+
     })
-  
+
   })
-  
+
   document.getElementById('userIMG').onchange = function () {
-        
+
     var fileinput = document.getElementById('userIMG');
 
     var f = fileinput.files[0];
@@ -51,19 +51,19 @@ $(document).ready(function () {
     var reader = new FileReader();
 
     reader.onload = function (e) {
-        
+
         $("#memeviewer").html('<img id="tempic" src="' + e.target.result + '">')
-        
+
         $("#tempic").attr("class", "centerer img-polaroid")
-        
+
     }
-    
+
     reader.readAsDataURL(f);
-    
+
   };
-  
+
   socket.on("nopost", function () {
     alert("You can only post once every 30 seconds...sorry this is to prevent malevolent button mashers :).")
   })
-  
+
 })
